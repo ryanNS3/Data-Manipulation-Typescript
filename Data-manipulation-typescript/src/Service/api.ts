e470da54b2ccb178e8ab9bc5dfc0ce4142d4a7bb
ryanNS3 <ryansantosn333@gmail.com>
2026-01-28 22:55:58 -0300

    feat/adicionando tratamento de erros

--- Data-manipulation-typescript/src/Service/api.ts
+++ Data-manipulation-typescript/src/Service/api.ts
@@ -1,7 +1,15 @@
 
 
-export async function fetchTransaction() {
-    const req =  await fetch("https://api.origamid.dev/json/transacoes.json")
-    console.log(req.json())
-    return await req.json()
+export async function fetchTransaction <T>(url: string): Promise<T | null> {
+    try{
+        const req =  await fetch(url)
+        if(!req.ok) throw new Error("Ocorreu um erro")
+        console.log(req.json())
+        return await req.json()
+
+    }
+
+    catch(err){
+        return null
+    }
 }
