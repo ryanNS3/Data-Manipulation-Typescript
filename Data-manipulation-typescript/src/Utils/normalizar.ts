4961a1a129b93bd09dfde88995983c7b3b0112c4
ryansantosn333 <ryansantosn333@gmail.com>
2026-01-30 14:35:03 +0000

    feat/função de normalização

--- Data-manipulation-typescript/src/Utils/normalizar.ts
+++ Data-manipulation-typescript/src/Utils/normalizar.ts
@@ -1,5 +1,22 @@
 
 
-export function normalizeJson(){
-    
+export function normalizeJson<T extends Record<string, any>>(data: T[]): Record<string, T[keyof T]>[] | null {
+
+    if (data){
+        return(
+            data.map((obj) =>{
+                const newObject: Record<string, T[keyof T]> = {}
+                Object.entries(obj).forEach(([key, value]) =>{
+                    const normalizeKey = key.toLowerCase().replace(/\s+/g, "_").replace(/[()]/g, "");
+                    newObject[normalizeKey] = value
+                })
+                return newObject
+            })
+           )
+    }
+
+    else{
+        return null
+    }
+ 
 }
