d38cebbc6e60afda93c9c3a7aa54a33b9231eb15
ryansantosn333 <ryansantosn333@gmail.com>
2026-01-30 14:34:47 +0000

    fix/ajuste no retorno da função de fetch

--- Data-manipulation-typescript/src/Service/api.ts
+++ Data-manipulation-typescript/src/Service/api.ts
@@ -2,10 +2,11 @@
 
 export async function fetchTransaction <T>(url: string): Promise<T | null> {
     try{
-        const req =  await fetch(url)
+        const req = await fetch(url)
         if(!req.ok) throw new Error("Ocorreu um erro")
-        console.log(req.json())
-        return await req.json()
+        const data = await req.json()
+
+        return data
 
     }
 
