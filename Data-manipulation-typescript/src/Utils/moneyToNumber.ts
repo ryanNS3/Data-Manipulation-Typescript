400c52af597c9ed637d5d168064a7435d866c7ca
ryanNS3 <ryansantosn333@gmail.com>
2026-02-03 23:26:19 -0300

    fix ajuste no replace da funcao

--- Data-manipulation-typescript/src/Utils/moneyToNumber.ts
+++ Data-manipulation-typescript/src/Utils/moneyToNumber.ts
@@ -1,3 +1,11 @@
 
 
-export function 
+export function moneyToNumber(money: string): number | null{
+    if (typeof money === "string"){
+        const moneyToNumber = Number(money.replace(",00","").replace("-", ""))
+        return moneyToNumber
+    }
+    else{
+        return null
+    }
+}
