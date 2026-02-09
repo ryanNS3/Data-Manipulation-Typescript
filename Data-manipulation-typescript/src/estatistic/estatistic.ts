9fdb75e6aeb74410ede7c7d32523b40fd34ad28a
Ryan <131712164+ryanNS3@users.noreply.github.com>
2026-02-09 18:31:19 -0300

    feat contabilização de tipo de transação

--- Data-manipulation-typescript/src/estatistic/estatistic.ts
+++ Data-manipulation-typescript/src/estatistic/estatistic.ts
@@ -17,12 +17,21 @@ import type { TransactionNormalized } from "../Type/transactionInterface";
 export function estatistic(data: TransactionNormalized[]){
     data.reduce((acc, atual) =>{
         acc.somaTotalValores = acc.somaTotalValores + atual.valor_r$;
+        if (atual.forma_de_pagamento == "Boleto"){
+            acc.transacaoBoleto += 1
+        }
+
+        else if (atual.forma_de_pagamento == "Cartão de crédito"){
+            acc.transacaoCredito += 1
+        }
+
+        
 
         return acc
     },{
         somaTotalValores: 0,
-        transacaoDebito:0,
         transacaoCredito:0,
+        transacaoBoleto:0,
         statusPago:0,
         statusRecusadoOperadoraCartoa:0,
         totalVendasPDia: [],
