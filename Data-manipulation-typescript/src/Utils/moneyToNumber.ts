da8942867ada2b2305e9a6424f89c1d626c0b67f
Ryan <131712164+ryanNS3@users.noreply.github.com>
2026-02-13 17:23:41 -0300

    refactor Ajustando o bug do "."

--- Data-manipulation-typescript/src/Utils/moneyToNumber.ts
+++ Data-manipulation-typescript/src/Utils/moneyToNumber.ts
@@ -1,18 +1,12 @@
-
-
-export function moneyToNumber(money: string): number | "-"{
-    if (money && typeof money === "string" && money != "-"){
-        const moneyToNumber = Number(money.replace(",","."))
-        return moneyToNumber
-    }
-    else if(money == "-"){
-        return money
-    }
-
-    else{
-        return "-"
-    }
-
-
-    
+export function moneyToNumber(money: string): number | "-" {
+  if (money && typeof money === "string" && money !== "-") {
+    const value = Number(
+      money.replace(/\./g, "").replace(",", ".")
+    );
+    return value;
+  } else if (money === "-") {
+    return money;
+  } else {
+    return "-";
+  }
 }
